799177c37f5e73c0089d0216a5570e7e42f73d66
CorneliusLKO3728 <lukecorners6@gmail.com>
2025-10-28 01:50:51 +0300

    deploy

--- viteconfig.js
+++ viteconfig.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-console.log('🚀 Vite config is loading!'); // Add this line
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://novels-api.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
-    },
+  build: {
+    outDir: 'dist'
   },
+  
 });
