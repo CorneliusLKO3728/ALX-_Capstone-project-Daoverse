aee699ed1f870b6e79f261eee65dbcd520abd84b
CorneliusLKO3728 <167845370+CorneliusLKO3728@users.noreply.github.com>
2025-10-28 12:48:41 +0300

    Update NovelDetail.jsx

--- src/pages/NovelDetail.jsx
+++ src/pages/NovelDetail.jsx
@@ -1,4 +1,4 @@
-// Replace the entire src/pages/NovelDetail.jsx with:
+
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { novelService } from '../services/novelService';
